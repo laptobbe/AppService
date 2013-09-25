b411bfd6cc605d02047110228b0a573bc8bf435e
Tobias Sundstrand <tobias@freeb.ee>
2013-09-25 07:44:09 +0200

    Initial Commit

---
+++ AppService/AppService.h
@@ -0,0 +1,13 @@
+//
+//  AppService.h
+//  AppService
+//
+//  Created by Tobias Sundstrand on 2013-09-25.
+//  Copyright (c) 2013 Computertalk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppService : NSObject
+
+@end
---
+++ AppService/AppService.m
@@ -0,0 +1,13 @@
+//
+//  AppService.m
+//  AppService
+//
+//  Created by Tobias Sundstrand on 2013-09-25.
+//  Copyright (c) 2013 Computertalk. All rights reserved.
+//
+
+#import "AppService.h"
+
+@implementation AppService
+
+@end
